1557885657ebd6d00fdaf74373fb2d119c226073
ShadeSWD <seawolfy60@gmail.com>
2023-08-25 13:20:33 +0300

    queries create tables

---
+++ src/database/queries.sql
@@ -0,0 +1,16 @@
+--Создание таблицы работодателей
+CREATE TABLE IF NOT EXISTS employers(
+    company_id INT PRIMARY KEY,
+    company_name VARCHAR(30) NOT NULL
+);
+
+--Создание таблицы вакансий
+CREATE TABLE IF NOT EXISTS vacancies(
+    vacancy_id SERIAL PRIMARY KEY,
+    company_id INT REFERENCES employers(company_id),
+    vacancy_name VARCHAR(100) NOT NULL,
+    vacancy_salary_from INTEGER NOT NULL,
+    vacancy_salary_to INTEGER NOT NULL,
+    vacancy_currency VARCHAR(10) NOT NULL,
+    vacancy_url VARCHAR(255) UNIQUE NOT NULL,
+)
